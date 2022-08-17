6a03d613b8a9ed629ffa3bd94e36a77ac402a8a7
8-Lambda-8 <j.wasle111@gmail.com>
2022-08-17 17:16:56 +0200

    implemented DMX recognition

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,19 @@
 #include <Arduino.h>
+#include <DMXSerial.h>
+
+#define relayPin 4
+#define switchTime 2000
 
 void setup() {
-  Serial.begin(57600);
+  DMXSerial.init(DMXReceiver);
+  
+  pinMode(relayPin, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  unsigned long lastPacket = DMXSerial.noDataSince();
+
+  digitalWrite(relayPin, lastPacket < switchTime);
+  digitalWrite(LED_BUILTIN, lastPacket < switchTime);
 }
